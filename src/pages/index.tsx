64fc57ea3b979908c95a6ebf93d2a7ae940988c1
Tagaiatto <34107323+Tagaiatto@users.noreply.github.com>
2021-05-20 14:37:26 -0300

    SSG

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import Head from "next/head";
-import { GetServerSideProps} from "next";
+import { GetStaticProps} from "next";
 import { SubscribeButton } from "../components/SubscribeButton";
 
 import styles from "./home.module.scss";
@@ -37,7 +37,7 @@ export default function Home({product}: HomeProps) {
   )
 }
 
-export const getServerSideProps: GetServerSideProps = async () => {
+export const getStaticProps: GetStaticProps = async () => {
   const price = await stripe.prices.retrieve('price_1ItBtXLqT4q66fiuakeQl311', {
      expand: ['product'] 
     }
@@ -54,6 +54,7 @@ export const getServerSideProps: GetServerSideProps = async () => {
   return {
     props: {
       product
-    }
+    },
+    revalidate: 60 * 60 * 24, // 24 hours
   }
 }
