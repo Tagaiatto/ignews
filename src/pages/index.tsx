69011c362d6b6e8e4b3b8928884726b49680fa43
Tagaiatto <34107323+Tagaiatto@users.noreply.github.com>
2021-05-20 09:31:54 -0300

    First commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,12 @@
+import Head from "next/head";
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>ignews</title>
+        <meta property="og:title" content="ignews" key="title" />
+      </Head>
+      <h1>Hello World</h1>
+    </>
+  )
+}
