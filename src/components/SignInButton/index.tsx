07dce7046e3a019aa8c318c0945a851ca76e4867
Tagaiatto <34107323+Tagaiatto@users.noreply.github.com>
2021-08-13 18:19:49 -0300

    SignInButton test

--- src/components/SignInButton/index.tsx
+++ src/components/SignInButton/index.tsx
@@ -1,6 +1,6 @@
-import {FaGithub} from "react-icons/fa";
-import {FiX} from "react-icons/fi";
-import {signIn, signOut, useSession} from "next-auth/client";
+import { FaGithub } from "react-icons/fa";
+import { FiX } from "react-icons/fi";
+import { signIn, signOut, useSession } from "next-auth/client";
 
 import styles from "./styles.module.scss";
 
@@ -8,7 +8,7 @@ export function SignInButton() {
   const [session] = useSession();
 
   return session ? (
-    <button 
+    <button
       type="button"
       className={styles.signInButton}
       onClick={() => signOut()}
@@ -18,13 +18,13 @@ export function SignInButton() {
       <FiX color="#737380" className={styles.closeIcon} />
     </button>
   ) : (
-    <button 
+    <button
       type="button"
       className={styles.signInButton}
       onClick={() => signIn('github')}
     >
       <FaGithub color="#eba417" />
-      Sign in with github
+      Sign in with Github
     </button>
   )
 }
