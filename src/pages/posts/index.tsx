768f4a3581d7322045be1806c35042aaf940409f
Tagaiatto <34107323+Tagaiatto@users.noreply.github.com>
2021-08-16 18:23:55 -0300

    Posts page tests

--- src/pages/posts/index.tsx
+++ src/pages/posts/index.tsx
@@ -17,8 +17,8 @@ interface PostsProps {
   posts: Post[];
 }
 
-export default function Posts({posts}: PostsProps) {
-  return(
+export default function Posts({ posts }: PostsProps) {
+  return (
     <>
       <Head>
         <title>Posts | ignews</title>
@@ -27,8 +27,8 @@ export default function Posts({posts}: PostsProps) {
       <main className={styles.container}>
         <div className={styles.posts}>
           {posts.map(post => (
-            <Link href={`/posts/${post.slug}`}>
-              <a key={post.slug}>
+            <Link key={post.slug} href={`/posts/${post.slug}`}>
+              <a>
                 <time>{post.updatedAt}</time>
                 <strong>{post.title}</strong>
                 <p>{post.excerpt}</p>
@@ -55,7 +55,7 @@ export const getStaticProps: GetStaticProps = async () => {
     return {
       slug: post.uid,
       title: RichText.asText(post.data.title),
-      excerpt: post.data.content.find( content => content.type === 'paragraph')?.text ?? '',
+      excerpt: post.data.content.find(content => content.type === 'paragraph')?.text ?? '',
       updatedAt: new Date(post.last_publication_date).toLocaleDateString('pt-BR', {
         day: '2-digit',
         month: 'long',
